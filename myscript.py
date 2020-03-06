7b34dee489e56e8bb09ec56671e20c551aef9937
Nichita Gruia <qdrg42JX87veDva>
2020-03-06 22:52:03 +0200

    Initial commit.

---
+++ myscript.py
@@ -0,0 +1,5 @@
+from some.module import words_to_string, add
+
+add(1, 2)
+words_to_string(['some', 'words'])
+words_to_string(('hello', 'python', 'community'))
---
+++ some/module.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a + b
+
+
+def words_to_string(words):
+    return ' '.join(words)
